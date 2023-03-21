2105fbf7902a12146550e805532dd9a1bf6c3b0f
Optimoos <dmarkd@gmail.com>
2023-03-21 17:52:20 -0400

    Split world into separate files
    Doing a little cleanup as the project grows, moved the world components to
    their own header/cpp files.

--- aints.h
+++ aints.h
@@ -8,51 +8,8 @@
 #include <cstdint>
 #include <vector>
 #include "raylib.h"
-#include "FastNoise/FastNoise.h"
-#include "BS_thread_pool.hpp"
+#include "world.h"
 
-class World
-{
-public:
-    enum BlockTypes {
-        kBlockAir,
-        kBlockDirt,
-        kBlockGrass,
-        kBlockFood,
-        kBlockStone,
-        kBlockWater,
-        kBlockSand,
-        kBlockUnderground
-    };
-
-    class Tile {
-    public:
-        static const uint16_t kTileX = 256;
-        static const uint16_t kTileY = 256;
-        std::vector<World::BlockTypes> blocks;
-        std::vector<float> noise_data_;
-        Texture2D tile_texture_;
-        void RegenerateTexture();
-
-   private:
-
-    };
-
-    World();
-    ~World();
-
-    BlockTypes GetBlockAtPos(int64_t x_pos, int64_t y_pos);
-    void SetBlockAtPos(int64_t x_pos, int64_t y_pos, World::BlockTypes type);
-    void AddFood(int64_t x_pos, int64_t y_pos, int64_t size);
-    Tile* PosToTile(int64_t x_pos, int64_t y_pos);
-    std::vector<std::vector<Tile>> world_tiles_;
-
-
-    const uint16_t kWorldX = 8192;
-    const uint16_t kWorldY = 2048;
-private:
-
-};
 
 class neuron
 {
@@ -95,10 +52,5 @@ private:
     //move_neuron* mn = new move_neuron;
     move_neuron mn;
 };
-
-void GenerateTileNoise(FastNoise::SmartNode<>& noise_generator, std::vector<float>& noise_data, uint16_t x_position, uint16_t y_position);
-std::vector<World::BlockTypes> NoiseToBlock(std::vector<float> noise);
-Texture2D GenerateTileTexture(std::vector<World::BlockTypes>& blocks);
-
 
 #endif //AIANTS_AINTS_H
---
+++ world.h
@@ -0,0 +1,55 @@
+#ifndef AIANTS_WORLD_H
+#define AIANTS_WORLD_H
+
+#include "BS_thread_pool.hpp"
+#include "FastNoise/FastNoise.h"
+#include "raylib.h"
+
+class World
+{
+public:
+    enum BlockTypes {
+        kBlockAir,
+        kBlockDirt,
+        kBlockGrass,
+        kBlockFood,
+        kBlockStone,
+        kBlockWater,
+        kBlockSand,
+        kBlockUnderground
+    };
+
+    class Tile {
+    public:
+        static const uint16_t kTileX = 256;
+        static const uint16_t kTileY = 256;
+        std::vector<World::BlockTypes> blocks;
+        std::vector<float> noise_data_;
+        Texture2D tile_texture_;
+        void RegenerateTexture();
+
+    private:
+
+    };
+
+    World();
+    ~World();
+
+    BlockTypes GetBlockAtPos(int64_t x_pos, int64_t y_pos);
+    void SetBlockAtPos(int64_t x_pos, int64_t y_pos, World::BlockTypes type);
+    void AddFood(int64_t x_pos, int64_t y_pos, int64_t size);
+    Tile* PosToTile(int64_t x_pos, int64_t y_pos);
+    std::vector<std::vector<Tile>> world_tiles_;
+
+
+    const uint16_t kWorldX = 8192;
+    const uint16_t kWorldY = 2048;
+private:
+
+};
+
+void GenerateTileNoise(FastNoise::SmartNode<>& noise_generator, std::vector<float>& noise_data, uint16_t x_position, uint16_t y_position);
+std::vector<World::BlockTypes> NoiseToBlock(std::vector<float> noise);
+Texture2D GenerateTileTexture(std::vector<World::BlockTypes>& blocks);
+
+#endif
